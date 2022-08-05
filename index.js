6fbadd8dcc782971119a3c88e70a0d6c0642a82b
JBestbier <106158849+JBestbier@users.noreply.github.com>
2022-08-05 17:00:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const appsRouter = require('./router/index.js');
+const appsRouter = require('https://gitcdn.link/cdn/JBestbier/App-Controler/main/routes/index.js');
 
 // Init Express
 const app = express();
